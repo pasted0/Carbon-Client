00f6f9cc75e080d980409b40c5e59db5108d77c7
pasted0 <pastedv1ontop@gmail.com>
2025-06-02 21:36:52 -0500

    update Games/126884695634066.lua

---
+++ Games/126884695634066.lua
@@ -0,0 +1,6 @@
+local vape = getgenv().vape
+local category = vape:CreateCategory({
+	Name = 'Combat',
+	Icon = getcustomasset('newvape/assets/combaticon.png'),
+	Size = UDim2.fromOffset(13, 14)
+})
